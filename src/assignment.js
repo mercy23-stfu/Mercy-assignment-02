52df01c2c8e09c7eb55955e0554ab7826dc9a395
Mercy Aremu <61360898+mercy23-stfu@users.noreply.github.com>
2022-07-11 16:39:44 +0000

    First Assignment: Set sum to 0 I initialize the variable to 0 and made it loop by a 1 increment and then add them to sum.
    Second Assignment:
    set countOfNumbers to a value of 0
    I initialize the variable to 0 and made it loop by a 1 increment and then add them to countOfNumbers.
    I used an if statement to ensure that that only count of even numbers are returned, using the remainder operator.

--- src/assignment.js
+++ src/assignment.js
@@ -3,34 +3,31 @@
 const assignment = {};
 // ========================
 
-
-/**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  
+let sum = 0;
+  
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
+  }
+     // return arrayOfNumbers.length;
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
 
-/**
- * Challenge - 2
- * 
- * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
+  assignment.sumOfNumbers = sumOfNumbers;
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+let count = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i ++){
+    if (
+      arrayOfNumbers[i] % 2 == 0
+    ) {
+      count++;
+    }
+  }
+    return count
+} 
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
